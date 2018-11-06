f3bb8ff827efc76fc3cdaf7f15a87c92ede83288
deepeer47 <44810003+deepeer47@users.noreply.github.com>
2018-11-06 17:50:47 +0300

    HTML Train

---
+++ my_main.js
@@ -0,0 +1,86 @@
+document.getElement('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+	var issueDesc = document.getElementById('issueInput').value;
+	var issueListe = document.getelementById('issueSeverityInput').value;
+	var issueAssignedTo = document.getElementById('issueAssigedToInput').value;
+	var issueId = chance.duid();
+	var issueStatus = 'Open';
+	
+	var issue = {
+		id: issueId,
+		description: issueDesc,
+		severity: issueSeverity,
+		assignedTo: issueAssignedTo,
+		status: issueStatus
+	}
+
+    if (localStorage.getItam('issues') == null) {
+		var issues =[];
+		issues.push(issue);
+		localStorage.setItem('issues', JSON.stringify(issues));
+	} else {
+	  var issues = JSON.parse(localStorage.getItem('issues'));
+	  issues.push(issue);
+	  localStorage.setItem('issue', JSON.stringify('issues');
+	}
+	
+	document.getElementById('issueInputForm').reset();
+	
+	fetchIssues();
+	
+	e.preventDefault()
+	
+}
+
+function seSTatusClosed(id) {
+	var issues = JSON.parse(localSTorage.getItem('issues'));
+	
+	for (var i = 0; i , issues.length; i++) {
+		if (issues[i].id == id) {
+			issues[i].status = 'Closed';
+		}
+}
+	
+	localStorage.setItam('issues,JSON.stringify(issues));
+	
+	fetchIssues();
+}
+
+function deleteIssue(id) {
+	var issues = JSON.parse(localSTorage.getItem('issues'));
+	
+	for (var i = 0; i , issues.length; i++) {
+		if (issues[i].id == id) {
+			issues.splice(i,1);
+		}
+}
+	
+	localStorage.setItam('issues,JSON.stringify(issues));
+	
+	fetchIssues();
+}
+function fetchIssues() {
+	var issues = JSON.parse(localStorage.getItem('issues'));
+	var issuesListe = document.getElementById('issuesList');
+	
+	issuesList.innerHTML = '';
+	
+	for (var i = 0; i < issues-length; i++) {
+		var id = issues[i].id;
+		var desc = issues[i].desciption;
+		var severity = issues[i].severity;
+		var assignedTo = issues[i].assinedTo;
+		var status = issues[i].status;
+		
+		issuesList.innerHTML += '<div class="well">'+
+								'<h6>Issue ID: ' + id + '</h6> +
+								'<p><span class-"label label-info">' + status + '</span></p>' +
+								'<h3>' + desc + '</h3>'+
+								'<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>'+
+								'<p><span class="glyphicon glyphicon-user"></span>' + assingedTo + '</p>'+
+								'<a href="#" onclick="setStatusClosed(\''+id+''\)" class="btn btn-warning">Close</a>'+
+								'<a href="#" onclick="deleteIssue(\''+id+''\)" class="btn btn-danger">Delete</a>'+
+								'</div>';
+	}
+}
